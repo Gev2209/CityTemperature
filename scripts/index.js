42a378a7b68308808de6c431fb82b7d175cb581b
Gevorg <mirzoyan.gevorg.23@gmail.com>
2023-05-31 01:36:38 +0400

    Modified js adding export import

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+import { API_KEY , API_URL , API_VERSION} from './constants.js';
+import { setErrorMessage, setEventById } from './helpers.js';
+
+const gettingWeather = () => {
+    const inputValue = document.getElementById('searchInput').value.trim();
+    if (inputValue) {
+        fetch(`${API_URL}/${API_VERSION}/weather?q=${inputValue}&appid=${API_KEY}&units=metric`)
+            .then((resp) => {
+                return resp.json();
+            })
+            .then(data => {
+                console.log(data);
+                document.getElementById('city').innerHTML = data.name;
+                document.getElementById('country').innerHTML = data.sys.country;
+                document.getElementById('temp').innerHTML = Math.round(data.main.temp);
+                setErrorMessage('')
+            })
+            .catch(() => {
+                console.log('request errrrrror');
+            })
+    } else {
+        setErrorMessage('Please enter the city name')
+    }
+}
+
+setEventById('searchButton', 'click', gettingWeather);
+
+const whichButton = (event) => {
+    if (event.code === 'Enter') {
+        gettingWeather()
+    }
+}
+
+setEventById('searchInput', 'keyup', whichButton)
